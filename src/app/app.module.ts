0dc90a1d9b916d2bde883b049e622753da863779
Yani92 <yanif13@outlook.com>
2019-09-05 19:49:57 -0500

    Arreglando Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ApolloModule } from "apollo-angular";
+import { HttpLinkModule } from "apollo-angular-link-http";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { NosotrosComponent } from './components/nosotros/nosotros.component';
-import { TurnosComponent} from './components/turnos/turnos.component';
-import { TiendaComponent } from './components/tienda/tienda.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RegistroClienteComponent } from './components/registro-cliente/registro-cliente.component';
-
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    InicioComponent,
-    NosotrosComponent,
-    TurnosComponent,
-    TiendaComponent,
-    RegistroClienteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
