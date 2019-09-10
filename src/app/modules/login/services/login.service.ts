6a44404829031c1cf4bbbcb8115068b507af8df9
Yani92 <yanif13@outlook.com>
2019-09-09 21:53:56 -0500

    trabajando en el login - consultas

---
+++ src/app/modules/login/services/login.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import gql from "graphql-tag";
+
+const LOGIN = gql`
+query iniciarSesion($username: String!, $password: String!){
+  login(username: $username, password: $password){
+    username
+    persona{
+      id
+      primerNombre
+      segundoNombre
+      primerApellido
+      segundoApellido
+    }
+  }
+}
+`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+}
