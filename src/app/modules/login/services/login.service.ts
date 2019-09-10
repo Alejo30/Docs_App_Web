bd357b4e43dc507a1c67ab84248df176c81625b3
Yani92 <yanif13@outlook.com>
2019-09-10 01:02:24 -0500

    Login funcionando

--- src/app/modules/login/services/login.service.ts
+++ src/app/modules/login/services/login.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import gql from "graphql-tag";
+import { Apollo } from 'apollo-angular';
+import { TryCatchStmt } from '@angular/compiler';
+import { UserWeb } from '../../../models/UserWeb';
 
 const LOGIN = gql`
 query iniciarSesion($username: String!, $password: String!){
@@ -21,5 +24,30 @@ query iniciarSesion($username: String!, $password: String!){
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private apollo:Apollo
+  ) { }
+
+  public async login(username: string, password: string):Promise<UserWeb>{
+    const query = await this.apollo.query(
+      {
+        query:LOGIN,
+        variables:{
+          username: username, //primera la variable de la consulta y segunda parametro
+          password: password
+        },
+        fetchPolicy:'network-only'
+      }
+    )
+    try {
+      const resultado = await query.toPromise();  
+      return resultado.data['login'] as UserWeb
+    } catch (error) {
+      //console.log("Usuario no encontrado");
+      return null;
+    }
+    
+    
+  }
+
 }
